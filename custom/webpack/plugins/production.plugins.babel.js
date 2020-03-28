5e7ba663a6482798e162d3c1cdc87aa29014a123
Mario Brusarosco <mariobrusarosco@hotmail.com>
2020-03-28 14:42:15 -0300

    Sets Bundle Analyzer

--- custom/webpack/plugins/production.plugins.babel.js
+++ custom/webpack/plugins/production.plugins.babel.js
@@ -1,5 +1,12 @@
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
-const productionPlugins = [new CleanWebpackPlugin()];
+const productionPlugins = [
+  new CleanWebpackPlugin(),
+  new BundleAnalyzerPlugin({
+    reportFilename: "../reports/bundle.html",
+    analyzerMode: "static"
+  })
+];
 
 export default productionPlugins;
