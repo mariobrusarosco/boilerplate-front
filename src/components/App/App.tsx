421b28157140933146cc0faaf98a2b63318b0e54
Mario Brusarosco <mariobrusarosco@hotmail.com>
2020-04-04 18:50:43 -0300

    Hot Reload

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,25 @@
+import { hot } from "react-hot-loader/root";
+import { useState } from "react";
+
 const App: React.FC = () => {
   console.log("App Component", APPLICATION);
-  return <main>App Component!</main>;
+
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => setCounter(counter + 1);
+
+  return (
+    <main>
+      <h1>App Component</h1>
+
+      <div>
+        <p>
+          Counter: <span>{counter}</span>
+        </p>
+        <button onClick={increment}>increment</button>
+      </div>
+    </main>
+  );
 };
 
-export default App;
+export default hot(App);
