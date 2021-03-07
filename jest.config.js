52ad85a519363c2a4aadea5ebfe916aff805f22f
Mario Brusarosco <mariobrusarosc@hotmail.com>
2021-03-07 17:29:30 -0300

    feat - jest initial config

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironmen: 'jsdom',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/'
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)'
+  ],
+  setupFilesAfterEnv: [
+    '<rootDir>/.jest/setup.ts'
+  ]
+}
