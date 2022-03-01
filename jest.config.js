068c10d520d87d3df930662acc408f3246da9a85
Mario Brusarosco <mariobrusarosco@hotmail.com>
2022-03-01 14:28:41 -0300

    chore: App creation via create-remix CLI

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // clearMocks: true,
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  moduleDirectories: ['node_modules', 'src']
-};
